ddaf0a92090f38807c166973325b2af7d70972b3
MauricioCombariza <mcombarizav@gmail.com>
2024-10-09 15:43:26 -0500

    commit "Ajustes al scanner"

--- components/BarCodeScannerComponent/index.tsx
+++ components/BarCodeScannerComponent/index.tsx
@@ -1,31 +1,90 @@
-// components/BarcodeScannerComponent.tsx
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
+import React, { ChangeEvent, FC, MouseEvent, useEffect, useRef, useState } from 'react';
+import { Layout } from '@/components/Layout';
+import { BrowserMultiFormatReader } from '@zxing/library';
 
-// Importa dinámicamente el componente de escaneo, desactivando SSR
-const BarcodeScannerComponent = dynamic(
-  () => import('react-qr-barcode-scanner'),
-  { ssr: false }
-);
 
-const BarcodeScanner: React.FC = () => {
-  const [data, setData] = useState<string | null>(null);
+const BarCodeScanner: FC = () => {
+  const [scanning, setScanning] = useState(false);
+  const [scanError, setScanError] = useState<string | null>(null); // Estado para manejar errores
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [guideNumber, setGuideNumber] = useState('');
+  
+  const handleGuideNumberChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setGuideNumber(event.target.value);
+  };
+
+  
+  useEffect(() => {
+    if (scanning) {
+      const codeReader = new BrowserMultiFormatReader();
+
+      const startScanning = async () => {
+        try {
+          await codeReader.decodeFromVideoDevice(null, videoRef.current!, (result) => {
+            if (result) {
+              setGuideNumber(result.getText());
+              alert(`Código QR escaneado: ${result.getText()}`);
+              setScanning(false);
+              setScanError(null); // Limpiar el error si el escaneo es exitoso
+            }
+          });
+        } catch (error) {
+          setScanError('Error al escanear el código de barras. Inténtalo nuevamente.');
+          console.error('Error en el escaneo de código de barras:', error);
+        }
+      };
+
+      startScanning();
+
+      return () => {
+        codeReader.reset();
+      };
+    }
+  }, [scanning, setGuideNumber]);
+
+  const startBarcodeScanner = () => {
+    setScanning(true);
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-xl font-bold mb-4">Escáner de Códigos de Barras</h1>
-      <div className="w-full max-w-md">
-        {/* Solo se renderiza en el lado del cliente */}
-        <BarcodeScannerComponent
-          onUpdate={(err, result) => {
-            if (result) setData(result.getText());
-            if (err) console.error(err);
-          }}
-        />
+    <Layout>
+      <div className="w-full max-w-md mx-auto mt-6">
+        <h1 className="bg-blue-500 text-2xl text-white text-center py-2 mb-4 mx-2">
+          Ingresar datos entrega
+        </h1>
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="guideNumber">
+              Número de guía:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              id="guideNumber"
+              value={guideNumber}
+              onChange={handleGuideNumberChange}
+            />
+          </div>
+
+          <button
+            className="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 mt-4 w-full rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={startBarcodeScanner}
+          >
+            Escanear Código de Barras
+          </button>
+        </form>
+
+        {/* Contenedor para el escaneo del código de barras */}
+        {scanning && (
+          <div>
+            <video ref={videoRef} style={{ width: '100%', height: '300px' }} autoPlay muted />
+            {scanError && <p className="text-red-500 text-center">{scanError}</p>}
+          </div>
+        )}
       </div>
-      {data && <p className="mt-4">Código escaneado: {data}</p>}
-    </div>
+    </Layout>
   );
 };
 
-export {BarcodeScanner};
+export {BarCodeScanner};
--- pages/BarCode/index.tsx
+++ pages/BarCode/index.tsx
@@ -1,12 +1,12 @@
 // pages/index.tsx
 
 import React from 'react';
-import { BarcodeScanner } from '@/components/BarCodeScannerComponent';
+import { BarCodeScanner } from '@/components/BarCodeScannerComponent';
 
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <BarcodeScanner />
+      <BarCodeScanner />
     </div>
   );
 };
